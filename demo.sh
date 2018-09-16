923251ff0049bd3395834b1ec2e8c028729e5a46
Flash <188813950@qq.com>
2018-09-17 00:51:58 +0800

    why must I commit some notes

---
+++ demo.sh
@@ -0,0 +1,7 @@
+mkdir $1
+cd $1
+mkdir css js
+touch css/style.css js/main.js
+echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+echo "h1{color: red;}" > css/style.css
+echo -e "var string = "Hello World"\nalert(string)" > js/main.js
