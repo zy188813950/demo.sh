a1054e652211e7b29fec81a271030d5cf72c6c1c
Flash <188813950@qq.com>
2018-09-17 01:04:03 +0800

    forget if exists

--- demo.sh
+++ demo.sh
@@ -1,7 +1,15 @@
-mkdir $1
-cd $1
-mkdir css js
-touch css/style.css js/main.js
-echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
-echo "h1{color: red;}" > css/style.css
-echo -e "var string = "Hello World"\nalert(string)" > js/main.js
+if [ -d $1 ]; then
+  echo "error: dir exists"  
+exit
+
+else
+ mkdir $1
+ cd $1
+ mkdir css js
+ touch css/style.css js/main.js
+ echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+ echo "h1{color: red;}" > css/style.css
+ echo -e "var string = "Hello World"\nalert(string)" > js/main.js
+ echo success
+ exit
+fi
